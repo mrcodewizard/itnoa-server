6a36f6e2dafd082d41d39c83f1c05835fff5535f
Mubashir Rahman <mubashirrahman503@gmail.com>
2022-08-25 17:37:32 +0500

    updated app.js for server

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 /*  build client and server index.html at start */
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "/client")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
